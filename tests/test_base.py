061b52a574b61163abc248e561c7a502cdb2d7b6
Raymond Ehlers <raymond.ehlers@gmail.com>
2018-11-23 01:02:07 -0500

    Make tests work (by sometimes ignoring ROOT)
    This way, we can perform nearly all of our tests without needing ROOT.
    This simplifies testing substantially.

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,71 +1,93 @@
 #!/usr/bin/env python
 
-# Tests for the various fit functions
-#
-# author: Raymond Ehlers <raymond.ehlers@cern.ch>, Yale University
-# date: 17 June 2018
+""" Tests for the base module.
 
+.. code-author: Raymond Ehlers <raymond.ehlers@cern.ch>, Yale University
+"""
+
+import logging
+import os
 import pytest
 import numpy as np
 import uproot
-import tempfile
-import logging
 
 import reactionPlaneFit.base as base
 
 logger = logging.getLogger(__name__)
 
 @pytest.fixture
-def setupHistToArray():
-    """ """
-    # This simulates
-    # hist.Fill(3, 2)
-    # hist.Fill(8)
-    # hist.Fill(8)
-    # hist.Fill(8)
-    # The error on bin 9 (one-indexed) is just sqrt(counts), while the error
-    # on bin 4 is sqrt(4) because we filled it with weight 2 (sumw2 squares this values).
+def setupHistogramConversion():
+    """ Setup expected values for histogram conversion tests.
+
+    This set of expected values corresponds to:
+
+    >>> hist = ROOT.TH1F("test", "test", 10, 0, 10)
+    >>> hist.Fill(3, 2)
+    >>> hist.Fill(8)
+    >>> hist.Fill(8)
+    >>> hist.Fill(8)
+
+    Note:
+        The error on bin 9 (one-indexed) is just sqrt(counts), while the error on bin 4
+        is sqrt(4) because we filled it with weight 2 (sumw2 squares this values).
+    """
     expected = base.Histogram(x = np.arange(1, 11) - 0.5,
                               y = np.array([0, 0, 0, 2, 0, 0, 0, 0, 3, 0]),
                               errors = np.array([0, 0, 0, np.sqrt(4), 0, 0, 0, 0, np.sqrt(3), 0]))
 
-    # ROOT hist
-    # This is the only ROOT dependence in the package
-    import rootpy.ROOT as ROOT
-    import rootpy.io
-    rootHist = ROOT.TH1F("rootHist", "rootHist", 10, 0, 10)
-    rootHist.Fill(3, 2)
-    for _ in range(3):
-        rootHist.Fill(8)
+    histName = "rootHist"
+    filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), "testFiles", "convertHist.root")
+    if not os.path.exists(filename):
+        # Need to create the initial histogram.
+        # This shouldn't happen very often, as the file is stored in the repository.
+        import ROOT
+        rootHist = ROOT.TH1F(histName, histName, 10, 0, 10)
+        rootHist.Fill(3, 2)
+        for _ in range(3):
+            rootHist.Fill(8)
 
-    # Uproot rootio read hist
-    with tempfile.NamedTemporaryFile() as f:
-        clonedHistName = "rootHistInFile"
-        with rootpy.io.root_open(f.name, "w"):
-            rootHistForFile = rootHist.Clone(clonedHistName)
-            rootHistForFile.Write()
+        # Write out with normal ROOT so we can avoid further dependencies
+        fOut = ROOT.TFile(filename, "RECREATE")
+        rootHist.Write()
+        fOut.Close()
 
-        # Now retrieve the hist from that file
-        uprootFile = uproot.open(f.name)
-        uprootHist = uprootFile[clonedHistName]
+    return filename, histName, expected
 
-    return (uprootHist, rootHist, expected)
+def checkHist(inputHist: base.Histogram, expected: base.Histogram) -> bool:
+    """ Helper function to compare a given Histogram against expected values.
 
-@pytest.mark.parametrize("histType", [
-    "uproot",
-    "ROOT",
-], ids = ["uproot", "ROOT"])
-def testUprootHistToArray(setupHistToArray, histType):
-    """ Test conversion of uproot hists to arrays. """
-    (uprootHist, rootHist, expected) = setupHistToArray
-
-    if histType == "uproot":
-        inputHist = uprootHist
-    elif histType == "ROOT":
-        inputHist = rootHist
-
+    Args:
+        inputHist (base.Histogram): Converted histogram.
+        expected (base.Histogram): Expected hiostgram.
+    Returns:
+        bool: True if the histograms are the same.
+    """
     h = base.Histogram.from_existing_hist(inputHist)
     np.testing.assert_allclose(h.x, expected.x)
     np.testing.assert_allclose(h.y, expected.y)
     np.testing.assert_allclose(h.errors, expected.errors)
+
+    return True
+
+@pytest.mark.root
+def testROOTHistToHistogram(setupHistogramConversion):
+    """ Check conversion of a read in ROOT file via ROOT to a Histogram object. """
+    filename, histName, expected = setupHistogramConversion
+
+    # Setup and read histogram
+    import ROOT
+    fIn = ROOT.TFile(filename, "READ")
+    inputHist = fIn.Get(histName)
+
+    assert checkHist(inputHist, expected) is True
+
+def testUprootHistToHistogram(setupHistogramConversion):
+    """ Check conversion of a read in ROOT file via uproot to a Histogram object. """
+    filename, histName, expected = setupHistogramConversion
+
+    # Retrieve the stored histogram via uproot
+    uprootFile = uproot.open(filename)
+    inputHist = uprootFile[histName]
+
+    assert checkHist(inputHist, expected) is True
 
